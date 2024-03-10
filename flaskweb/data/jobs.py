8a759190879b2957faffc1c63e53a06dee3d26d5
Pavel <151254599+PavelFr8@users.noreply.github.com>
2024-03-10 23:33:45 +0300

    some goods for db(which i will make in the future)

---
+++ flaskweb/data/jobs.py
@@ -0,0 +1,17 @@
+from .db_session import SqlAlchemyBase
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
+
+
+class Jobs(SqlAlchemyBase):
+    __tablename__ = 'jobs'
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    team_leader = sa.Column(sa.Integer, sa.ForeignKey("users.id"))
+    job = sa.Column(sa.String)
+    work_size = sa.Column(sa.Integer)
+    collaborators = sa.Column(sa.String)
+    start_date = sa.Column(sa.DateTime)
+    end_date = sa.Column(sa.DateTime)
+    is_finished = sa.Column(sa.Boolean)
+
+    user = orm.relationship('User')
--- flaskweb/data/users.py
+++ flaskweb/data/users.py
@@ -1,28 +1,29 @@
 from .db_session import SqlAlchemyBase
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
-import datetime
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
-from sqlalchemy_serializer import SerializerMixin
 
-class User(SqlAlchemyBase, UserMixin, SerializerMixin):
+
+class User(SqlAlchemyBase):
     __tablename__ = 'users'
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
-    name = sa.Column(sa.String, nullable=True)
-    about = sa.Column(sa.String)
-    hashed_password = sa.Column(sa.String, nullable=True)
-    email = sa.Column(sa.String, nullable=True, unique=True, index=True)
-    created_date = sa.Column(sa.DateTime, default=datetime.datetime.now)
+    surname = sa.Column(sa.String)
+    name = sa.Column(sa.String)
+    age = sa.Column(sa.Integer)
+    position = sa.Column(sa.String)
+    speciality = sa.Column(sa.String)
+    address = sa.Column(sa.String)
+    hashed_password = sa.Column(sa.String)
+    email = sa.Column(sa.String, unique=True)
+    modified_date = sa.Column(sa.DateTime)
 
-    news = orm.relationship('News', back_populates='user')
+    users = orm.relationship('Jobs', back_populates='user')
 
     def __repr__(self) -> str:
         return f'<{self.id}> {self.name} {self.email}'
-    
 
-    def set_password(self, password):
-        self.hashed_password = generate_password_hash(password)
 
-    def check_password(self, password):
-        return check_password_hash(self.hashed_password, password)
+
+
+
+
+
